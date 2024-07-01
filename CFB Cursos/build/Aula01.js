c055912957ee9cca2fdf58210e26863edab6b4de
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-01 11:17:50 -0300

    Preparando o Ambiente

---
+++ CFB Cursos/build/Aula01.js
@@ -0,0 +1,6 @@
+"use strict";
+const nome = "Jhonn";
+const curso = "CFB Cursos";
+const curso1 = "Typescript";
+console.log(nome);
+console.log(curso);
---
+++ CFB Cursos/src/Aula01.ts
@@ -0,0 +1,16 @@
+// Ts Precisa compilar para funcionar na web Js
+// Porem Funciona com o node.js
+
+// Compilacao = tsc nomedoarquivo 
+// Apos comlilar e abri os dois arquivos juntos retornara
+// um erro que tem ja achou a variavel no mesmo lugar 
+
+// Programaçao Fortemento tipada
+
+const nome = "Jhonn";
+const curso = "CFB Cursos";
+const curso1 = "Typescript" // Qualquer Alteracao tera que compilar novamente
+
+
+console.log(nome);
+console.log(curso);
