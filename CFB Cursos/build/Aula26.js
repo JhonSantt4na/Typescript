bc24478764ae90a3a2d4aae026e3850e061f1285
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-04 20:05:56 -0300

    Aula26 Interfaces

--- CFB Cursos/build/Aula26.js
+++ CFB Cursos/build/Aula26.js
@@ -1,9 +1,33 @@
 "use strict";
-let novo_curso;
-novo_curso = {
+let novo_curso1;
+novo_curso1 = {
     titulo: "Typescript",
     des: "Curso de Typescript",
     aula: 100,
+    qtddAlunos: 60
+};
+console.log(novo_curso1);
+let curso11;
+let curso22;
+let curso33;
+curso11 = {
+    titulo: "Javascript",
+    des: "Curso de JS",
+    aula: 266,
     qtddAlunos: 50
 };
-console.log(curso);
+curso22 = {
+    titulo: "HTML",
+    des: "Curso de HTML",
+    aula: 150,
+    qtddAlunos: 50
+};
+curso33 = {
+    titulo: "CSS",
+    des: "Curso de CSS",
+    aula: 100,
+    qtddAlunos: 33
+};
+console.log(curso11);
+console.log(curso22);
+console.log(curso33);
