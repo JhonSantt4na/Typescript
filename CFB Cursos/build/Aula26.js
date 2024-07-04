bc74fa945afb3138e66bfdf8788fe20e262ca492
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-04 19:52:46 -0300

    Aula 26

---
+++ CFB Cursos/build/Aula26.js
@@ -0,0 +1,9 @@
+"use strict";
+let novo_curso;
+novo_curso = {
+    titulo: "Typescript",
+    des: "Curso de Typescript",
+    aula: 100,
+    qtddAlunos: 50
+};
+console.log(curso);
---
+++ CFB Cursos/src/Aula26.ts
@@ -0,0 +1,22 @@
+// Interfaces Objeto Literais
+let novo_curso:{
+    //Definindo propriedade
+    titulo:string;
+    des:string;
+    aula:number;
+    qtddAlunos:number;
+}
+// Podemos definir varias propriedades
+// Passar o objeto como parametro com varias propriedades
+
+novo_curso = {
+    titulo:"Typescript",
+    des:"Curso de Typescript",
+    aula:100,
+    qtddAlunos:50
+}
+
+
+
+
+console.log(curso);
