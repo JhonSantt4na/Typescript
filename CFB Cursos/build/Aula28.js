5fb17b887383010f06ac9b99f0f61c97ca478a41
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-04 20:58:36 -0300

    Aula28 Generics

---
+++ CFB Cursos/build/Aula28.js
@@ -0,0 +1,13 @@
+"use strict";
+function f_testte(valor, r) {
+    return r;
+}
+console.log(f_testte(12, "Como"));
+class C_teste {
+    valor;
+    constructor(valor) {
+        this.valor = valor;
+    }
+}
+const ct1 = new C_teste(55);
+console.log(ct1.valor);
