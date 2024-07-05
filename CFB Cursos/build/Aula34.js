22425a4177a021ffb6f8942331620a3e14efef9e
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-05 20:42:47 -0300

    Aula 34 Desestruturação

---
+++ CFB Cursos/build/Aula34.js
@@ -0,0 +1,14 @@
+"use strict";
+let ary = [10, 20, 30, 40];
+let collor = ["Azul", "Branco", "Vermelho", "Verde"];
+const objet = {
+    cor1: "verde",
+    cor2: "Azivis",
+    cor3: "Red",
+    cor4: "Preto"
+};
+let { cor1, cor2, cor3, cor4 } = objet;
+console.log(cor1);
+console.log(cor2);
+console.log(cor3);
+console.log(cor4);
